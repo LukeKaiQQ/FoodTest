719ae95d1accce9a2cf178d283cb0e99ae95705f
LAPTOP-PEUPEMQQ\asus <enjoylife88911@gmail.com>
2022-01-21 02:16:28 +0800

    Revert "commit"
    This reverts commit d68eb177828841f5e8cea43b9f04b44b41803bad.

--- src/main/java/tw/com/fcb/foodTest/FoodService.java
+++
@@ -1,9 +0,0 @@
-package tw.com.fcb.foodTest;
-
-import java.util.List;
-
-public interface FoodService {
-	public Food getFoodID(String foodID);
-	
-	public List<Food> getFoodName(String foodName);
-}
